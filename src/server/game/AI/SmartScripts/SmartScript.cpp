f1c9e2e94738236d48828a1301a345fda094fd64
Jelle Meeus <sogladev@gmail.com>
2024-06-19 01:49:39 +0200

    add case for instant cast ranged attacks

--- src/server/game/AI/SmartScripts/SmartScript.cpp
+++ src/server/game/AI/SmartScripts/SmartScript.cpp
@@ -639,7 +639,7 @@ void SmartScript::ProcessAction(SmartScriptHolder& e, Unit* unit, uint32 var0, u
                     bool isSpellIgnoreLOS = spellInfo->HasAttribute(SPELL_ATTR2_IGNORE_LINE_OF_SIGHT);
 
                     // If target is rooted we move out of melee range before casting, but not further than spell max range.
-                    if (isWithinLOSInMap && isWithinMeleeRange && isRangedAttack && isTargetRooted && (canCastSpell || canCastRangedWeaponSpell) && !isInstantCast)
+                    if (isWithinLOSInMap && isWithinMeleeRange && isRangedAttack && isTargetRooted && (canCastSpell || canCastRangedWeaponSpell) && (!isInstantCast || isRangedAttack))
                     {
                         failedSpellCast = true; // Mark spellcast as failed so we can retry it later
                         float minDistance = std::max(meleeRange, spellMinRange) - distanceToTarget + NOMINAL_MELEE_RANGE;
