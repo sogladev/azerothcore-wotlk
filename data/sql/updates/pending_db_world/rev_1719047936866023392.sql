ecf4e439354851e02e19bf5571b8f907975a1a6d
Jelle Meeus <sogladev@gmail.com>
2024-06-22 11:19:44 +0200

    boss_mother_shahraz aura:spell_mother_shahraz_random_periodic_aura

--- data/sql/updates/pending_db_world/rev_1719047936866023392.sql
+++ data/sql/updates/pending_db_world/rev_1719047936866023392.sql
@@ -140,3 +140,4 @@ UPDATE `spell_script_names` SET `ScriptName`='spell_q10923_evil_draws_near_perio
 UPDATE `spell_script_names` SET `ScriptName`='spell_mark_of_kazzak_aura' WHERE `spell_id`=32960;
 UPDATE `spell_script_names` SET `ScriptName`='spell_npc22275_crystal_prison_aura' WHERE `spell_id`=40846;
 UPDATE `spell_script_names` SET `ScriptName`='spell_q10563_q10596_to_legion_hold_aura' WHERE `spell_id`=37097;
+UPDATE `spell_script_names` SET `ScriptName`='spell_mother_shahraz_random_periodic_aura' WHERE `spell_id`=40867;
