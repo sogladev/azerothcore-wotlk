c1933f24e17d6866a39046a71744bdb9ed80d03c
Jelle Meeus <sogladev@gmail.com>
2024-06-22 11:19:19 +0200

    zone_dragonblight aura:spell_q12243_fire_upon_the_waters_aura

--- data/sql/updates/pending_db_world/rev_1719047936866023392.sql
+++ data/sql/updates/pending_db_world/rev_1719047936866023392.sql
@@ -2,3 +2,4 @@
 UPDATE `spell_script_names` SET `ScriptName`='spell_q11919_q11940_drake_hunt_aura' WHERE `spell_id`=46620;
 UPDATE `spell_script_names` SET `ScriptName`='spell_ioc_repair_turret_aura' WHERE `spell_id`=68077;
 UPDATE `spell_script_names` SET `ScriptName`='spell_ioc_parachute_ic_aura' WHERE `spell_id`=66656;
+UPDATE `spell_script_names` SET `ScriptName`='spell_q12243_fire_upon_the_waters_aura' WHERE `spell_id`=48522;
