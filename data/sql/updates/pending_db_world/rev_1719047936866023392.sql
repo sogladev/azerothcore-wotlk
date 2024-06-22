8f0917907f37d5a7058bfee9060a46ef2d1da726
Jelle Meeus <sogladev@gmail.com>
2024-06-22 11:19:31 +0200

    boss_professor_putricide aura:spell_putricide_unbound_plague_dmg_aura

--- data/sql/updates/pending_db_world/rev_1719047936866023392.sql
+++ data/sql/updates/pending_db_world/rev_1719047936866023392.sql
@@ -85,3 +85,4 @@ UPDATE `spell_script_names` SET `ScriptName`='spell_deathwhisper_mana_barrier_au
 UPDATE `spell_script_names` SET `ScriptName`='spell_putricide_grow_stacker_aura' WHERE `spell_id`=70345;
 UPDATE `spell_script_names` SET `ScriptName`='spell_putricide_gaseous_bloat_aura' WHERE `spell_id` IN (70672,72455,72832,72833);
 UPDATE `spell_script_names` SET `ScriptName`='spell_putricide_mutated_plague_aura' WHERE `spell_id` IN (72451,72463,72671,72672);
+UPDATE `spell_script_names` SET `ScriptName`='spell_putricide_unbound_plague_dmg_aura' WHERE `spell_id` IN (70911,72854,72855,72856);
