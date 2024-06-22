d3e0645c052191d2dadf92de4ebf58be55b425e4
Jelle Meeus <sogladev@gmail.com>
2024-06-22 11:19:28 +0200

    boss_blood_queen_lana_thel aura:spell_blood_queen_essence_of_the_blood_queen_aura

--- data/sql/updates/pending_db_world/rev_1719047936866023392.sql
+++ data/sql/updates/pending_db_world/rev_1719047936866023392.sql
@@ -69,3 +69,4 @@ UPDATE `spell_script_names` SET `ScriptName`='spell_tharon_ja_dummy_aura' WHERE 
 UPDATE `spell_script_names` SET `ScriptName`='spell_dred_grievious_bite_aura' WHERE `spell_id`=48920;
 UPDATE `spell_script_names` SET `ScriptName`='spell_blood_queen_pact_of_the_darkfallen_dmg_aura' WHERE `spell_id`=71340;
 UPDATE `spell_script_names` SET `ScriptName`='spell_blood_queen_frenzied_bloodthirst_aura' WHERE `spell_id` IN (70877,71474);
+UPDATE `spell_script_names` SET `ScriptName`='spell_blood_queen_essence_of_the_blood_queen_aura' WHERE `spell_id`=70871;
