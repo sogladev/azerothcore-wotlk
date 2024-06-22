a160d6dcfeb4e5ac351f8fe13d10ddd8661365c1
Jelle Meeus <sogladev@gmail.com>
2024-06-22 11:19:25 +0200

    boss_halion aura:spell_halion_leave_twilight_realm_aura

--- data/sql/updates/pending_db_world/rev_1719047936866023392.sql
+++ data/sql/updates/pending_db_world/rev_1719047936866023392.sql
@@ -46,3 +46,4 @@ UPDATE `spell_script_names` SET `ScriptName`='spell_halion_meteor_strike_spread_
 UPDATE `spell_script_names` SET `ScriptName`='spell_halion_soul_consumption_aura' WHERE `spell_id`=74792;
 UPDATE `spell_script_names` SET `ScriptName`='spell_halion_mark_of_combustion_aura' WHERE `spell_id`=74567;
 UPDATE `spell_script_names` SET `ScriptName`='spell_halion_twilight_realm_aura' WHERE `spell_id`=74807;
+UPDATE `spell_script_names` SET `ScriptName`='spell_halion_leave_twilight_realm_aura' WHERE `spell_id`=74812;
