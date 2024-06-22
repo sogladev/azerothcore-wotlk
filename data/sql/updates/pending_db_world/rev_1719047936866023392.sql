ce81bf06c83f5b72e02828ea9618bac54b1ade7a
Jelle Meeus <sogladev@gmail.com>
2024-06-22 11:19:32 +0200

    boss_sindragosa aura:spell_sindragosa_instability_aura

--- data/sql/updates/pending_db_world/rev_1719047936866023392.sql
+++ data/sql/updates/pending_db_world/rev_1719047936866023392.sql
@@ -88,3 +88,4 @@ UPDATE `spell_script_names` SET `ScriptName`='spell_putricide_mutated_plague_aur
 UPDATE `spell_script_names` SET `ScriptName`='spell_putricide_unbound_plague_dmg_aura' WHERE `spell_id` IN (70911,72854,72855,72856);
 UPDATE `spell_script_names` SET `ScriptName`='spell_putricide_mutated_transformation_dismiss_aura' WHERE `spell_id` IN (70405,72508,72509,72510);
 UPDATE `spell_script_names` SET `ScriptName`='spell_sindragosa_permeating_chill_aura' WHERE `spell_id`=70107;
+UPDATE `spell_script_names` SET `ScriptName`='spell_sindragosa_instability_aura' WHERE `spell_id`=69766;
