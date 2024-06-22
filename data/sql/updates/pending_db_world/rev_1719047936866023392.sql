5ace1473e4a0df80585b41d33a0380d7e9729ed4
Jelle Meeus <sogladev@gmail.com>
2024-06-22 11:19:23 +0200

    brann_bronzebeard aura:spell_hos_dark_matter_aura

--- data/sql/updates/pending_db_world/rev_1719047936866023392.sql
+++ data/sql/updates/pending_db_world/rev_1719047936866023392.sql
@@ -27,3 +27,4 @@ UPDATE `spell_script_names` SET `ScriptName`='spell_frost_tomb_aura' WHERE `spel
 UPDATE `spell_script_names` SET `ScriptName`='spell_voa_overcharge_aura' WHERE `spell_id`=64217;
 UPDATE `spell_script_names` SET `ScriptName`='spell_koralon_meteor_fists_aura' WHERE `spell_id` IN (66725,68161);
 UPDATE `spell_script_names` SET `ScriptName`='spell_flame_warder_meteor_fists_aura' WHERE `spell_id` IN (66808,68160);
+UPDATE `spell_script_names` SET `ScriptName`='spell_hos_dark_matter_aura' WHERE `spell_id`=51001;
