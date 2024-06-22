9e14c375a4351e6b19137b6a783bc678d332ae1b
Jelle Meeus <sogladev@gmail.com>
2024-06-22 11:19:20 +0200

    zone_grizzly_hills aura:spell_infected_worgen_bite_aura

--- data/sql/updates/pending_db_world/rev_1719047936866023392.sql
+++ data/sql/updates/pending_db_world/rev_1719047936866023392.sql
@@ -6,3 +6,4 @@ UPDATE `spell_script_names` SET `ScriptName`='spell_q12243_fire_upon_the_waters_
 UPDATE `spell_script_names` SET `ScriptName`='spell_q13003_thursting_hodirs_spear_aura' WHERE `spell_id`=56689;
 UPDATE `spell_script_names` SET `ScriptName`='spell_close_rift_aura' WHERE `spell_id`=56763;
 UPDATE `spell_script_names` SET `ScriptName`='spell_wintergrasp_hide_small_elementals_aura' WHERE `spell_id`=52107;
+UPDATE `spell_script_names` SET `ScriptName`='spell_infected_worgen_bite_aura' WHERE `spell_id`=53094;
