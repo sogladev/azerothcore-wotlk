b0a400a8b7ff9361e78b00a6d7541933ff94275e
Jelle Meeus <sogladev@gmail.com>
2024-06-22 11:19:28 +0200

    icecrown_citadel aura:spell_icc_stoneform_aura

--- data/sql/updates/pending_db_world/rev_1719047936866023392.sql
+++ data/sql/updates/pending_db_world/rev_1719047936866023392.sql
@@ -70,3 +70,4 @@ UPDATE `spell_script_names` SET `ScriptName`='spell_dred_grievious_bite_aura' WH
 UPDATE `spell_script_names` SET `ScriptName`='spell_blood_queen_pact_of_the_darkfallen_dmg_aura' WHERE `spell_id`=71340;
 UPDATE `spell_script_names` SET `ScriptName`='spell_blood_queen_frenzied_bloodthirst_aura' WHERE `spell_id` IN (70877,71474);
 UPDATE `spell_script_names` SET `ScriptName`='spell_blood_queen_essence_of_the_blood_queen_aura' WHERE `spell_id`=70871;
+UPDATE `spell_script_names` SET `ScriptName`='spell_icc_stoneform_aura' WHERE `spell_id`=70733;
