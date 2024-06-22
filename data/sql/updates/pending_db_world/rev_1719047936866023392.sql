380234ebe9c43abd9a710a63fdbb583d25961b93
Jelle Meeus <sogladev@gmail.com>
2024-06-22 11:19:20 +0200

    zone_wintergrasp aura:spell_wintergrasp_hide_small_elementals_aura

--- data/sql/updates/pending_db_world/rev_1719047936866023392.sql
+++ data/sql/updates/pending_db_world/rev_1719047936866023392.sql
@@ -5,3 +5,4 @@ UPDATE `spell_script_names` SET `ScriptName`='spell_ioc_parachute_ic_aura' WHERE
 UPDATE `spell_script_names` SET `ScriptName`='spell_q12243_fire_upon_the_waters_aura' WHERE `spell_id`=48522;
 UPDATE `spell_script_names` SET `ScriptName`='spell_q13003_thursting_hodirs_spear_aura' WHERE `spell_id`=56689;
 UPDATE `spell_script_names` SET `ScriptName`='spell_close_rift_aura' WHERE `spell_id`=56763;
+UPDATE `spell_script_names` SET `ScriptName`='spell_wintergrasp_hide_small_elementals_aura' WHERE `spell_id`=52107;
