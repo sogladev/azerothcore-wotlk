6b78bbc4239f0894640655aea3e2779c20e881a0
Jelle Meeus <sogladev@gmail.com>
2024-06-22 11:19:37 +0200

    boss_icecrown_gunship_battle aura:spell_igb_on_gunship_deck_aura

--- data/sql/updates/pending_db_world/rev_1719047936866023392.sql
+++ data/sql/updates/pending_db_world/rev_1719047936866023392.sql
@@ -105,3 +105,4 @@ UPDATE `spell_script_names` SET `ScriptName`='spell_igb_rocket_pack_useable_aura
 UPDATE `spell_script_names` SET `ScriptName`='spell_igb_explosion_main_aura' WHERE `spell_id` IN (72134,72137);
 UPDATE `spell_script_names` SET `ScriptName`='spell_igb_periodic_trigger_with_power_cost_aura' WHERE `spell_id` IN (69470,69487);
 UPDATE `spell_script_names` SET `ScriptName`='spell_igb_overheat_aura' WHERE `spell_id`=69487;
+UPDATE `spell_script_names` SET `ScriptName`='spell_igb_on_gunship_deck_aura' WHERE `spell_id` IN (70120,70121);
