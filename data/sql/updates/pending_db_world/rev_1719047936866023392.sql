3ff2ab86059ed0c77c838f14d56e5a3ef977c582
Jelle Meeus <sogladev@gmail.com>
2024-06-22 11:19:25 +0200

    violet_hold aura:spell_destroy_door_seal_aura

--- data/sql/updates/pending_db_world/rev_1719047936866023392.sql
+++ data/sql/updates/pending_db_world/rev_1719047936866023392.sql
@@ -49,3 +49,4 @@ UPDATE `spell_script_names` SET `ScriptName`='spell_halion_twilight_realm_aura' 
 UPDATE `spell_script_names` SET `ScriptName`='spell_halion_leave_twilight_realm_aura' WHERE `spell_id`=74812;
 UPDATE `spell_script_names` SET `ScriptName`='spell_halion_twilight_cutter_periodic_aura' WHERE `spell_id`=74768;
 UPDATE `spell_script_names` SET `ScriptName`='spell_optic_link_aura' WHERE `spell_id`=54396;
+UPDATE `spell_script_names` SET `ScriptName`='spell_destroy_door_seal_aura' WHERE `spell_id`=58040;
