3a023020b317abb6e4c068a748b0ec44e8425ea6
Jelle Meeus <sogladev@gmail.com>
2024-06-22 11:19:39 +0200

    instance_scholomance aura:spell_scholomance_boon_of_life_aura

--- data/sql/updates/pending_db_world/rev_1719047936866023392.sql
+++ data/sql/updates/pending_db_world/rev_1719047936866023392.sql
@@ -112,3 +112,4 @@ UPDATE `spell_script_names` SET `ScriptName`='spell_rotface_unstable_ooze_explos
 UPDATE `spell_script_names` SET `ScriptName`='spell_shadowfang_keep_haunting_spirits_aura' WHERE `spell_id`=7057;
 UPDATE `spell_script_names` SET `ScriptName`='spell_shadowfang_keep_forsaken_skills_aura' WHERE `spell_id`=7054;
 UPDATE `spell_script_names` SET `ScriptName`='spell_scholomance_fixate_aura' WHERE `spell_id`=12021;
+UPDATE `spell_script_names` SET `ScriptName`='spell_scholomance_boon_of_life_aura' WHERE `spell_id`=17179;
