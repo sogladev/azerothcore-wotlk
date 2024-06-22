c50a5b45907bda7ec527092cd8e194a0ee88ed63
Jelle Meeus <sogladev@gmail.com>
2024-06-22 11:19:26 +0200

    boss_anubarak_trial aura:spell_gen_leeching_swarm_aura

--- data/sql/updates/pending_db_world/rev_1719047936866023392.sql
+++ data/sql/updates/pending_db_world/rev_1719047936866023392.sql
@@ -56,3 +56,4 @@ UPDATE `spell_script_names` SET `ScriptName`='spell_hadronox_leech_poison_aura' 
 UPDATE `spell_script_names` SET `ScriptName`='spell_toc25_mistress_kiss_aura' WHERE `spell_id` IN (66334,67905,67906,67907);
 UPDATE `spell_script_names` SET `ScriptName`='spell_faction_champion_warl_unstable_affliction_aura' WHERE `spell_id` IN (65812,68154,68155,68156);
 UPDATE `spell_script_names` SET `ScriptName`='spell_pursuing_spikes_aura' WHERE `spell_id` IN (65920,65922,65923);
+UPDATE `spell_script_names` SET `ScriptName`='spell_gen_leeching_swarm_aura' WHERE `spell_id` IN (66118,67630,68646,68647);
