68ec0fc4345077912de70861c13e5ab9e60fabf2
Jelle Meeus <sogladev@gmail.com>
2024-06-22 11:19:29 +0200

    icecrown_citadel aura:spell_icc_yf_frozen_orb_aura

--- data/sql/updates/pending_db_world/rev_1719047936866023392.sql
+++ data/sql/updates/pending_db_world/rev_1719047936866023392.sql
@@ -73,3 +73,4 @@ UPDATE `spell_script_names` SET `ScriptName`='spell_blood_queen_essence_of_the_b
 UPDATE `spell_script_names` SET `ScriptName`='spell_icc_stoneform_aura' WHERE `spell_id`=70733;
 UPDATE `spell_script_names` SET `ScriptName`='spell_icc_web_wrap_aura' WHERE `spell_id`=70980;
 UPDATE `spell_script_names` SET `ScriptName`='spell_icc_dark_reckoning_aura' WHERE `spell_id`=69483;
+UPDATE `spell_script_names` SET `ScriptName`='spell_icc_yf_frozen_orb_aura' WHERE `spell_id`=71274;
