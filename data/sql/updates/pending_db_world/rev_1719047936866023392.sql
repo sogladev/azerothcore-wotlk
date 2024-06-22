bdac270181c445fc145b8da6de637f2aec21927c
Jelle Meeus <sogladev@gmail.com>
2024-06-22 11:19:31 +0200

    boss_professor_putricide aura:spell_putricide_gaseous_bloat_aura

--- data/sql/updates/pending_db_world/rev_1719047936866023392.sql
+++ data/sql/updates/pending_db_world/rev_1719047936866023392.sql
@@ -83,3 +83,4 @@ UPDATE `spell_script_names` SET `ScriptName`='spell_dreamwalker_summon_suppresse
 UPDATE `spell_script_names` SET `ScriptName`='spell_valithria_suppression_aura' WHERE `spell_id`=70588;
 UPDATE `spell_script_names` SET `ScriptName`='spell_deathwhisper_mana_barrier_aura' WHERE `spell_id`=70842;
 UPDATE `spell_script_names` SET `ScriptName`='spell_putricide_grow_stacker_aura' WHERE `spell_id`=70345;
+UPDATE `spell_script_names` SET `ScriptName`='spell_putricide_gaseous_bloat_aura' WHERE `spell_id` IN (70672,72455,72832,72833);
