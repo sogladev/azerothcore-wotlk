f32216404d96c262474d98c6ab430402c97863eb
Jelle Meeus <sogladev@gmail.com>
2024-06-22 11:19:40 +0200

    boss_kiljaeden aura:spell_kiljaeden_vengeance_of_the_blue_flight_aura

--- data/sql/updates/pending_db_world/rev_1719047936866023392.sql
+++ data/sql/updates/pending_db_world/rev_1719047936866023392.sql
@@ -121,3 +121,4 @@ UPDATE `spell_script_names` SET `ScriptName`='spell_geddon_armageddon_aura' WHER
 UPDATE `spell_script_names` SET `ScriptName`='spell_threatening_gaze_aura' WHERE `spell_id`=24314;
 UPDATE `spell_script_names` SET `ScriptName`='spell_kiljaeden_shadow_spike_aura' WHERE `spell_id`=46680;
 UPDATE `spell_script_names` SET `ScriptName`='spell_kiljaeden_darkness_aura' WHERE `spell_id`=46605;
+UPDATE `spell_script_names` SET `ScriptName`='spell_kiljaeden_vengeance_of_the_blue_flight_aura' WHERE `spell_id`=45839;
