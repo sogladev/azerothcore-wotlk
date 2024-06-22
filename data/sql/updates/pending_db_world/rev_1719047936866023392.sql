99b38584648e524be39db2c9d828321d921b6627
Jelle Meeus <sogladev@gmail.com>
2024-06-22 11:19:23 +0200

    boss_hodir aura:spell_hodir_storm_power_aura

--- data/sql/updates/pending_db_world/rev_1719047936866023392.sql
+++ data/sql/updates/pending_db_world/rev_1719047936866023392.sql
@@ -28,3 +28,4 @@ UPDATE `spell_script_names` SET `ScriptName`='spell_voa_overcharge_aura' WHERE `
 UPDATE `spell_script_names` SET `ScriptName`='spell_koralon_meteor_fists_aura' WHERE `spell_id` IN (66725,68161);
 UPDATE `spell_script_names` SET `ScriptName`='spell_flame_warder_meteor_fists_aura' WHERE `spell_id` IN (66808,68160);
 UPDATE `spell_script_names` SET `ScriptName`='spell_hos_dark_matter_aura' WHERE `spell_id`=51001;
+UPDATE `spell_script_names` SET `ScriptName`='spell_hodir_storm_power_aura' WHERE `spell_id` IN (63711,65134);
