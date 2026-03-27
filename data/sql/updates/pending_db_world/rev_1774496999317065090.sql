27c6539181f4bf69dea05f183572e7af677f0367
sogla <sogladev@gmail.com>
2026-03-27 09:02:11 +0100

    fixup! fix(Scripts/Northrend): improve EoE encounter visuals

--- data/sql/updates/pending_db_world/rev_1774496999317065090.sql
+++ data/sql/updates/pending_db_world/rev_1774496999317065090.sql
@@ -1,2 +1,2 @@
 --
-UPDATE `creature_template` SET `faction` = 14, `npcflag` = 16777216, `unit_flags` = 33554432 WHERE (`entry` = 30248);
+UPDATE `creature_template` SET `faction` = 14, `npcflag` = 16777216, `unit_flags` = 33554432 WHERE (`entry` IN (30248, 31749));
